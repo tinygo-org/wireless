c5efbcb11cf192a6c51830dfdf704b3771aef41e
deadprogram <ron@hybridgroup.com>
2025-12-10 20:40:06 +0100

    examples: add wspr example
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/wspr/noradio.go
@@ -0,0 +1,26 @@
+//go:build !si5351 && !featherwing
+
+package main
+
+import "tinygo.org/x/wireless/fsk4"
+
+func initRadio() *fsk4.FSK4 {
+	return fsk4.NewFSK4(&NoRadio{}, 144800000, 5000, 1200)
+}
+
+type NoRadio struct{}
+
+func (r *NoRadio) Transmit(freq uint64) error {
+	println("NoRadio Transmit called with freq:", freq)
+	return nil
+}
+
+func (r *NoRadio) Standby() error {
+	println("NoRadio Standby called")
+	return nil
+}
+
+func (r *NoRadio) Close() error {
+	println("NoRadio Close called")
+	return nil
+}
